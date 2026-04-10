392bc928afa2822729955c2ca7f5999dca2377c1
andreoliveiralang <andreoliveiralang@gmail.com>
2026-04-09 22:51:51 -0300

    commit

---
+++ helloworld.py
@@ -0,0 +1,37 @@
+print("_______________________________________________________")
+
+print ("Hello World") # aceita tanto aspas duplas e aspas simples, escolha uma e siga com ela.
+print() #podemos usar tanto o "print()" ou "\n" para quebrar a linha, quando quisermos. o \n so funciona dentro de uma String.
+
+
+#PARA O USUÁRIO DIGITAR OS DADOS:
+name = input("please enter your name: ")
+idade = input("please enter your age: ")
+print (name,idade)
+
+print("_______________________________________________________")
+#IMPRIMINDO ESPAÇOS BRANCO
+# print() e \n
+print("podemos por exemplo quebrar a linha bem aqui\nveja que com isso a nossa frase\nvai se quebrando.")
+
+print("_______________________________________________________")
+#CONTAS DE MATEMATICA, FAZENDO RESOLUÇAO.
+print ("Adicionando Números")
+x = 5 * 10
+print("fazendo divisão")
+y = x / 3
+print("Matemática completa")
+
+print("_______________________________________________________") 
+
+# é muito bom habito adicionar comentarios, porque muitos codigos esquecemos qual funçao ele esta exercendo em especifico, então é de bom tom que vc escreva, para que dps de algum tempo, fique fácil de ler o código.
+
+
+
+# O Grande diferencial do PYTHON, é não precisar delcarrar variaveis, então se é DOUBLE, INT,FLOAT, STRING, não precisa, e isso é muito bom.
+
+primeiroNome = "Andre"
+ultimoNome = "Lang"
+print("Hello "+ primeiroNome +" "+ ultimoNome)
+print("_______________________________________________________") 
+
